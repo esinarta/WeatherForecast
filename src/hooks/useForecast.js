b278d8f90cc07f7f34f77176c013d83b6c66aa61
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-10-23 01:08:59 -0700

    Refactored useForecast and removed console logs

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -6,35 +6,38 @@ const useForecast = (city) => {
   const [forecast, setForecast] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  console.log(city);
+
   useEffect(() => {
-    axios.get(`${API_URL}lat=${city.lat}&lon=${city.lon}`
-      + `&exclude=current,minutely,hourly,alerts`
-      + `&units=metric&appid=${API_KEY}`
-    ).then((res) => {
-      console.log(res);
-      const days = res.data.daily.map((day) => {
-        return {
-          id: new Date(day.dt * 1000).getTime(),
-          date: new Date(day.dt * 1000).toLocaleDateString('en-US', {
-            weekday: 'short',
-            month: 'short',
-            day: 'numeric',
-          }),
-          maxTemp: Math.round(day.temp.max),
-          minTemp: Math.round(day.temp.min),
-          weatherDesc: day.weather[0].description,
-          weatherIconUrl: `${ICON_URL}${day.weather[0].icon}@2x.png`
-        }
-      });
-      setForecast(days);
-      setLoading(false);
-    })
-    .catch((err) => {
-      setError(err);
-      setLoading(false);
-    })
-  },[city.lat, city.lon]);
+    const getForecast = async () => {
+      try {
+        const res = await axios.get(`${API_URL}lat=${city.lat}&lon=${city.lon}`
+        + `&exclude=current,minutely,hourly,alerts`
+        + `&units=metric&appid=${API_KEY}`);
+
+        const days = res.data.daily.map((day) => {
+          return {
+            id: new Date(day.dt * 1000).getTime(),
+            date: new Date(day.dt * 1000).toLocaleDateString('en-US', {
+              weekday: 'short',
+              month: 'short',
+              day: 'numeric',
+            }),
+            maxTemp: Math.round(day.temp.max),
+            minTemp: Math.round(day.temp.min),
+            weatherDesc: day.weather[0].description,
+            weatherIconUrl: `${ICON_URL}${day.weather[0].icon}@2x.png`
+          }
+        });
+
+        setForecast(days);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+    getForecast();
+  }, [city.lat, city.lon]);
 
   return { forecast, loading, error };
 };
